5c858691783f1ccfbfb738bcfcf0832a7ebec778
Ismael Martínez <121949680+ISMARZDEV@users.noreply.github.com>
2025-10-12 17:41:14 -0400

    fix: corregir el formato y agregar clases de color en el componente App (#4)

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,22 @@
-
-import { Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const App = () => {
   return (
     <SafeAreaView>
-      <View className='mt-6 mx-2.5'>
-        <Text className="text-2xl font-bold mb-4 font-work-black">
+      <View className="mt-6 mx-2.5 ">
+        <Text className="text-2xl font-bold mb-4 font-work-black text-primary">
           ¡Hola Mundo!
         </Text>
-         <Text className="text-4xl font-bold mb-4 font-work-medium">
+        <Text className="text-4xl font-bold mb-4 font-work-medium text-secondary-100">
           ¡Hola Mundo!
         </Text>
-         <Text className="text-5xl font-bold mb-4 font-work-light">
+        <Text className="text-5xl font-bold mb-4 font-work-light text-tertiary">
           ¡Hola Mundo!
         </Text>
       </View>
     </SafeAreaView>
   );
-}
+};
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,27 @@ module.exports = {
     //Se aplicara unicamente en cualquier archivo del directorio (carpeta)de app || components || presentation
     "./app/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
-    "./presentation/**/*.{js,jsx,ts,tsx}"
+    "./presentation/**/*.{js,jsx,ts,tsx}",
   ],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
+      colors: {
+        primary: "#49129c",
+        secondary: {
+          DEFAULT: "#B40086",
+          100: "#C51297",
+          200: "#831266",
+        },
+        tertiary: "#EF2967",
+      },
+
       fontFamily: {
-        'work-black': ['WorkSans-Black', 'sans-serif'],
-        'work-light': ['WorkSans-Light', 'sans-serif'],
-        'work-medium': ['WorkSans-Medium', 'sans-serif'],
-      }
+        "work-black": ["WorkSans-Black", "sans-serif"],
+        "work-light": ["WorkSans-Light", "sans-serif"],
+        "work-medium": ["WorkSans-Medium", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
-
+};
