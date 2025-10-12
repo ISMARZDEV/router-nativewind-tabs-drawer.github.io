e635400b311b03c981c3f98833c589e7d3de691d
Ismael Martínez <1077546@est.intec.edu.do>
2025-10-12 17:27:16 -0400

    feat: agregar soporte para fuentes personalizadas y mejorar el diseño del componente principal

--- app/index.tsx
+++ app/index.tsx
@@ -4,9 +4,15 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 const App = () => {
   return (
-    <SafeAreaView className='flex-1'>
-      <View className='mt-10'>
-        <Text className="text-2xl font-bold mb-4">
+    <SafeAreaView>
+      <View className='mt-6 mx-2.5'>
+        <Text className="text-2xl font-bold mb-4 font-work-black">
+          ¡Hola Mundo!
+        </Text>
+         <Text className="text-4xl font-bold mb-4 font-work-medium">
+          ¡Hola Mundo!
+        </Text>
+         <Text className="text-5xl font-bold mb-4 font-work-light">
           ¡Hola Mundo!
         </Text>
       </View>
