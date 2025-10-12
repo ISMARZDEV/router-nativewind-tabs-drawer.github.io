d2f729b43a06c98e67e4bfff61ecbd208a708bf7
Ismael Martínez <121949680+ISMARZDEV@users.noreply.github.com>
2025-10-12 18:17:03 -0400

    feat: agregar pantallas de navegación para Productos, Perfil y Ajustes (#5)

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,26 @@
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Redirect } from "expo-router";
 
 const App = () => {
-  return (
-    <SafeAreaView>
-      <View className="mt-6 mx-2.5 ">
-        <Text className="text-2xl font-bold mb-4 font-work-black text-primary">
-          ¡Hola Mundo!
-        </Text>
-        <Text className="text-4xl font-bold mb-4 font-work-medium text-secondary-100">
-          ¡Hola Mundo!
-        </Text>
-        <Text className="text-5xl font-bold mb-4 font-work-light text-tertiary">
-          ¡Hola Mundo!
-        </Text>
-      </View>
-    </SafeAreaView>
-  );
+  return <Redirect href="/home" />;
+
+  // return (
+  //   <SafeAreaView>
+  //     <View className="mt-6 mx-2.5 ">
+  //       <Text className="text-2xl font-bold mb-4 font-work-black text-primary">
+  //         ¡Hola Mundo!
+  //       </Text>
+  //       <Text className="text-4xl font-bold mb-4 font-work-medium text-secondary-100">
+  //         ¡Hola Mundo!
+  //       </Text>
+  //       <Text className="text-5xl font-bold mb-4 font-work-light text-tertiary">
+  //         ¡Hola Mundo!
+  //       </Text>
+
+  //       <Link href='/products'>Productos</Link>
+
+  //     </View>
+  //   </SafeAreaView>
+  // );
 };
 
 export default App;
