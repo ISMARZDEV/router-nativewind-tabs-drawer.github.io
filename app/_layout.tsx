9c017074cff944ed199ca540c995b21ce72f2724
Ismael Martínez <121949680+ISMARZDEV@users.noreply.github.com>
2025-10-12 17:28:35 -0400

    feat: agregar soporte para fuentes personalizadas y mejorar el diseño del componente principal (#3)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,27 @@
-import { Slot } from 'expo-router'
-import React from 'react'
+import { Slot, SplashScreen } from 'expo-router'
+import React, { useEffect } from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import "./global.css"
+import { useFonts } from 'expo-font';
 
 const RootLayout = () => {
+
+   SplashScreen.preventAutoHideAsync(); 
+
+   const [ fontsLoaded, error ] = useFonts({
+    'WorkSans-Black': require('../assets/fonts/WorkSans-Black.ttf'),
+    'WorkSans-Light': require('../assets/fonts/WorkSans-Light.ttf'),
+    'WorkSans-Medium': require('../assets/fonts/WorkSans-Medium.ttf'),
+    })
+    
+    useEffect(() => {
+        if( error ) throw error;
+
+        if(fontsLoaded) SplashScreen.hideAsync();
+    }, [fontsLoaded, error]);
+
+    if (!fontsLoaded && !error) return null;
+
   return (
     <SafeAreaProvider>
       <Slot />
