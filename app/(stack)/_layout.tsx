ffcde6e821b10d16de7b4b198e02e40d6fe231e5
Ismael Martínez <121949680+ISMARZDEV@users.noreply.github.com>
2025-10-12 23:29:13 -0400

    feat: mejorar la estructura del componente StackLayout y ajustar opciones de navegación (#10)

--- app/(stack)/_layout.tsx
+++ app/(stack)/_layout.tsx
@@ -3,13 +3,34 @@ import React from "react";
 
 const StackLayout = () => {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+
+      headerShown: true,
+      headerShadowVisible: false,
+      headerBackVisible: false,
+      animation: "slide_from_right",
+      contentStyle: {
+        backgroundColor: 'white'
+      },
+      headerTitleAlign: 'center',
+      }}
+    >
       <Stack.Screen
-        name="home/index"
-        options={{
-          title: "Inicio",
-        }}
+      name="home/index"
+      options={{
+        title: "Inicio",
+      }}
+      
       />
+
+      <Stack.Screen name="products/index"
+      options={{
+        title: "perfil",
+      }}
+      >
+
+      </Stack.Screen>
 
       <Stack.Screen
         name="profile/index"
