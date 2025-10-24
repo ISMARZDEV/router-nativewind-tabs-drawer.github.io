d44e5d7752b3097c05c85c5b6cdc80f574aa60cf
Ismael Martínez <1077546@est.intec.edu.do>
2025-10-24 09:59:23 -0400

    feat: agregar pantallas de productos y detalles, incluyendo la lógica de navegación y datos de productos

--- app/(stack)/products/index.tsx
+++ app/(stack)/products/index.tsx
@@ -1,12 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { products } from "@/store/products.store";
+import { Link } from "expo-router";
+import { FlatList, Text, View } from "react-native";
 
 const ProductsScreen = () => {
   return (
-    <View>
-      <Text>ProductsScreen</Text>
-    </View>
-  )
-}
+    <View className="flex flex-1 px-4">
+      <FlatList
+        data={products}
+        keyExtractor={(items) => items.id}
+        renderItem={({ item }) => (
+          <View className="mt-10">
+            <Text className="text-2xl font-work-black">{item.title}</Text>
+            <Text className="">{item.description}</Text>
 
-export default ProductsScreen
+            <View className="flex flex-row justify-between mt-2">
+              <Text className="font-work-black">{item.price}</Text>
+              <Link
+                href={`/(stack)/products/${item.id}`}
+                className="text-primary"
+              >
+                Ver detalles
+              </Link>
+            </View>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+export default ProductsScreen;
